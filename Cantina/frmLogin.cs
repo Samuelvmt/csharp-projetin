b2affb8fbba25c86f8de433a3209022d525408d2
Samuelvmt <145499760+Samuelvmt@users.noreply.github.com>
2024-08-23 22:11:16 -0300

    Add files via upload
    projeto cantinas

---
+++ Cantina/frmLogin.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cantina
+{
+    public partial class frmLogin : Form
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        public void limparCampos (){
+            txtUsuario.Clear();
+            txtSenha.Text = "";
+            txtUsuario.Focus();
+        }
+
+        private void btnEntrar_Click(object sender, EventArgs e)
+        {
+            string usuario, senha;
+
+            usuario = txtUsuario.Text;
+            senha = txtSenha.Text;
+
+            if (usuario.Equals("rapi") && senha.Equals("do"))
+            {
+                frmMenuPrincipal abrir = new frmMenuPrincipal();
+                abrir.Show();
+                this.Hide();
+
+            }
+            else
+            {
+                MessageBox.Show("Usuário ou senha incorretos!","Mensagem do Sistema",MessageBoxButtons.OK, MessageBoxIcon.Information,MessageBoxDefaultButton.Button1);
+                limparCampos();
+            }
+
+
+                }
+
+        private void frmLogin_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
